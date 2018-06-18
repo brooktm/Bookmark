d924746650f69487e10ea592613910b70cb8b5ca
brooktm <brooktm@gmail.com>
2018-06-18 12:08:01 -0700

    fetch the saved bookmarks from local storage

--- js/app.js
+++ js/app.js
@@ -13,22 +13,54 @@ function saveBookmark(e){
         url: siteUrl
     }
 
-if (localStorage.getItem('bookmarks') === null){ // tes if bookmark is null
+if (localStorage.getItem('bookmarks') === null) {
+    // Init array
     var bookmarks = [];
+    // Add to array
     bookmarks.push(bookmark);
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks)); // if bookmark is not there add to local storage
+    // Set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 } else {
-    // get bookmarks from local storage
-
+    // Get bookmarks from localStorage
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    //add the bookmarks to array
-
+    // Add bookmark to array
     bookmarks.push(bookmark);
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 }
 
+ document.getElementById('myForm').reset();
 
+ // Re-fetch bookmarks
+ fetchBookmarks();
 
 //prevent form from submiting 
     e.preventDefault();
    
 }
+
+
+
+//fetch bookmarks
+
+function fetchBookmarks() {
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Get output id
+    var bookmarksResults = document.getElementById('bookmarksResults');
+
+    // Build output
+    bookmarksResults.innerHTML = '';
+    for (var i = 0; i < bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">' +
+            '<h3>' + name +
+            ' <a class="btn btn-default" target="_blank" href="' + url + '">Visit</a> ' +
+            ' <a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger" href="#">Delete</a> ' +
+            '</h3>' +
+            '</div>';
+    }
+}
+
